5b480971cfa19f9fd6e6c5fabc4a5b89036dc3f4
tthrasher <tthrasher@gmail.com>
2021-04-29 16:25:45 -0400

    Initial Commit

---
+++ TimesTables/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimesTables
+//
+//  Created by Terry Thrasher on 2021-04-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimesTables/TimesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesApp.swift
+//  TimesTables
+//
+//  Created by Terry Thrasher on 2021-04-29.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
