e9cc0347b9704c67893d1a20721ddd793d859b8d
tthrasher <tthrasher@gmail.com>
2021-05-03 18:58:06 -0400

    Creating second view and generating questions

--- TimesTables/ContentView.swift
+++ TimesTables/ContentView.swift
@@ -34,14 +34,16 @@ struct TimesTableQuestion: View {
 }
 
 struct ContentView: View {
-    @State private var promptingForSettings = true
+    @State private var promptingForSettings = false
     @State private var selectedNumberOfQuestions = 0
-    @State private var correctAnswers = 0
+    @State private var score = 0
     @State private var multiplier = 0
-    @State private var secondNumber = 0
     
     @State private var numberOfQuestions = ["5", "10", "20", "All"]
     @State private var questions = [String]()
+    @State private var correctAnswers = [Int]()
+    @State private var currentQuestion = ""
+    @State private var currentAnswer = ""
     
     var body: some View {
         NavigationView {
@@ -60,10 +62,10 @@ struct ContentView: View {
                             
                         }
                         
-                        Section(header: Text("What is the number you want to multiply by?")) {
+                        Section(header: Text("Which times tables do you want to do?")) {
                             Picker("Multiplier", selection: $multiplier) {
-                                ForEach(0..<10) {
-                                    Text("\($0 + 3)")
+                                ForEach(0..<12) {
+                                    Text("\($0 + 1)")
                                 }
                             }
                             .pickerStyle(SegmentedPickerStyle())
@@ -72,10 +74,8 @@ struct ContentView: View {
                         
                         Spacer()
                         
-                        
                         Button("Let's go!") {
                             promptingForSettings.toggle()
-                            
                             // play the game
                         }
                         .frame(maxWidth: 200, maxHeight: 50)
@@ -86,20 +86,92 @@ struct ContentView: View {
                         .padding()
                         
                         
-                        Text("Score: \(correctAnswers)")
+                        Text("Score: \(score)")
                     }
-                    .navigationTitle("TimesTables")
+                }
+                    
+                else {
+                    VStack {
+                        Text("Here come the questions!")
+                            .font(.largeTitle)
+                            .padding(50)
+                            
+                        Text("\(currentQuestion)")
+                            .font(.title2)
+                            .padding(50)
+                            
+                        TextField("Your answer", text: $currentAnswer)
+                            .keyboardType(.decimalPad)
+                            .padding(20)
+                        
+                        Button("Check my answer") {
+                            // validate answer
+                        }
+                        .frame(maxWidth: 240, maxHeight: 50)
+                        .overlay(Rectangle().stroke(Color.black, lineWidth: 3))
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .padding()
+            
+                    }
                 }
             }
         }
     }
     
-    func askTimesTablesQuestions(questions: Int, multiplier: Int) {
+    func askTimesTablesQuestions(numberOfQuestions: Int, multiplier: Int) {
         // take in the parameters and generate the questions, then shuffle the questions
         // show the questions one at a time and prompt for an answer in a text field
         // validate the answer and remove the question from the array; update score
         // when there are no questions remaining, present summary and return to settings view
+        
+        
+        switch numberOfQuestions {
+        case 0:
+            generateQuestions(number: 5)
+            
+            for i in 1...5 {
+                currentQuestion = questions[i]
+            }
+            
+        case 1:
+            generateQuestions(number: 10)
+            
+            for i in 1...10 {
+                currentQuestion = questions[i]
+            }
+            
+        case 2:
+            generateQuestions(number: 20)
+            
+            for i in 1...20 {
+                currentQuestion = questions[i]
+            }
+            
+        default:
+            generateQuestions(number: 30)
+            
+            for i in 1...30 {
+                currentQuestion = questions[i]
+            }
+        }
     }
+    
+    func generateQuestions(number: Int) {
+        var multiplicands = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+        
+        for i in 1...number {
+            multiplicands.shuffle()
+            let multiplicand = multiplicands[i]
+            let correctAnswer = multiplier * multiplicand
+            correctAnswers.append(correctAnswer)
+            let question = "What is \(multiplier) times \(multiplicand)?"
+            questions.append(question)
+        }
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
